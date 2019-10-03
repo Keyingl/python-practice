ee7968a8e74e6e488795c9d965de042691cb99bb
Keyingl <50162500+Keyingl@users.noreply.github.com>
2019-10-03 01:37:57 -0700

    first assignment in my python class

---
+++ numbers.py
@@ -0,0 +1,8 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+the_sum = first_number + second_number
+print("The sum is:\n",the_sum,sep='')
+the_product = first_number * second_number
+print("The product is:\n",the_product,sep='')
+
+
---
+++ time.py
@@ -0,0 +1,4 @@
+a=int(input("Enter a number of seconds: "))
+b=int(a/60)
+c=int(a%60)
+print("That is",b,"minutes and",c,"seconds.",sep=' ')
